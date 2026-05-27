67e10787e1376ccdb45c1504ac14e294ed4ccea2
LakshmiVisalakshi25 <24A95A0517@aec.edu.in>
2026-05-27 20:38:19 +0530

    Fixed CORS for mobile

--- Backend/server.js
+++ Backend/server.js
@@ -68,7 +68,13 @@ const transporter =
 // ===============================
 // MIDDLEWARE
 // ===============================
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // ===============================
