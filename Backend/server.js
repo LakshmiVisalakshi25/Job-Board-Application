257e6906aab8c2b298106465e9fb8e27f6c75b11
LakshmiVisalakshi25 <24A95A0517@aec.edu.in>
2026-05-28 20:25:41 +0530

    Fixed nodemailer SMTP timeout

--- Backend/server.js
+++ Backend/server.js
@@ -51,13 +51,23 @@ const otpStore = {};
 // ===============================
 const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+  host: "smtp.gmail.com",
+
+  port: 587,
+
+  secure: false,
 
   auth: {
 
     user: process.env.EMAIL_USER,
 
     pass: process.env.EMAIL_PASS,
+
+  },
+
+  tls: {
+
+    rejectUnauthorized: false,
 
   },
 
